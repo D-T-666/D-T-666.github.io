1b054e91a8178c15335755a564d5a0f2c47b131d
D-T-666 <35934791+D-T-666@users.noreply.github.com>
2021-06-19 13:59:02 +0000

    Deploying to gh-pages from @ D-T-666/D-T-666.github.io@6d702d0edda4a4ea96ee604d0f5b300ac596846c 🚀

--- js/script.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const file = window.location.hash.substr(1) || "homepage";
-
-  fetch(`content/${file}.html`)
-    .then((data) => data.text())
-    .then((content) => {
-      document.getElementById("content").innerHTML = content;
-    });
-})();
